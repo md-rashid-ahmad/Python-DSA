ac9bbfb4de72b9e2b6d33d8c3e503cbf1833d93f
Md Rashid Ahmad <131335264+md-rashid-ahmad@users.noreply.github.com>
2024-06-23 17:05:07 +0530

    Create assignment-operators.py

---
+++ Day-3/CodingExercises/assignment-operators.py
@@ -0,0 +1,14 @@
+# assignment
+
+marks = 89    # here in this line we are assigning "89" to the "variable" marks
+print(marks)
+marks += 10   # here in this line we are increasing marks value by 10
+print(marks)
+marks -= 10   # here in this line we are decreasing marks value by 10
+print(marks)
+marks *= 2
+print(marks)
+marks /= 2
+print(marks)
+marks %= 2
+print(marks)
