d95e7362a9d117d6e68c5f9463e8b85120c03053
Md Rashid Ahmad <131335264+md-rashid-ahmad@users.noreply.github.com>
2024-06-23 17:42:43 +0530

    Create logical & bitwise.py

---
+++ Day-3/CodingExercises/logical & bitwise.py
@@ -0,0 +1,27 @@
+# logical operators
+
+print(True and True)
+print(True and False)
+print(False and False)
+print(True or True)
+print(True or False)
+print(False or False)
+
+xor = True ^ True
+print(xor)
+xor = True ^ False
+print(xor)
+xor = False ^ False
+print(xor)
+
+
+# bitwise Operator
+
+num1 = 10
+num2 = 20
+
+print(num1 & num2)
+print(num1 | num2)
+print(num1 ^ num2)
+print(num1 << 2)
+print(num1 >> 2)
