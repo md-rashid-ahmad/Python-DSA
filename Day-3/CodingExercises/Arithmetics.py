f52577f0681b6643367d28beb9c5098456531f48
Md Rashid Ahmad <131335264+md-rashid-ahmad@users.noreply.github.com>
2024-06-23 16:36:09 +0530

    Update and rename exercise1.py to Arithmetics.py

---
+++ Day-3/CodingExercises/Arithmetics.py
@@ -0,0 +1,57 @@
+# Arithmetics Operator
+
+num1 = 100
+num2 = 20
+
+sum = num1 + num2
+print(sum)
+
+# Subtraction
+
+sub = num1 - num2
+print(sub)
+
+# Multiplication
+
+mul = num1 * num2
+print(mul)
+
+# Division
+
+div = num1 / num2
+print(div)
+
+# Modulus
+
+mod = num1 % num2
+print(mod)
+
+# Questiont
+
+quest = num1 // num2
+print(quest)
+
+# Exponent
+
+exp = num1 ** num2
+print(exp)
+
+remainder = num1 % num2
+print(remainder)
+
+num = int(input("Enter the number "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# positive infinity
+
+print(float('inf'))
+
+# negative infinity
+
+print(-float('inf'))
+
+nans = float('nan')
+print(nans)
